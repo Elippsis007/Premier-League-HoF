88bedbea8576b981c1083fb085eed10a4c5b6a77
Elippsis007 <michaeldrmm@gmail.com>
2022-01-29 18:31:56 +0000

    pushing changes from bag to cart

--- pl_hof/settings.py
+++ pl_hof/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
-    'bag',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bag.contexts.bag_contents',
+                'cart.contexts.cart_contents',
             ],
         },
     },
