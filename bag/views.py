8dff1e8587acc1a58bcfed5c339878c0f3ec5f7b
Elippsis007 <michaeldrmm@gmail.com>
2022-01-30 17:13:48 +0000

    adding message levels in base.html

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -10,7 +13,8 @@ def view_bag(request):
 # A view that takes both the request and the item_id which is the id of the product the user would like to add to the bag
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    
+    product = Product.objects.get(pk=item_id)
 # Getting the quantity from the form, it has to be converted to an interger since it will come from the template as a string
     quantity = int(request.POST.get('quantity'))
 # I want to get the redirect URL from the form so I know where to redirect once the process is finished
@@ -38,16 +42,65 @@ def add_to_bag(request, item_id):
             bag[item_id] = {'items_by_size': {size: quantity}}
     # if there is no size I can run the following
     else:
-
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
     
     request.session['bag'] = bag
     return redirect(redirect_url)
     
 
+def adjust_bag(request, item_id):
+    """ Adjusting the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+   
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size'][size]:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Adjusting the quantity of the specified product to the specified amount """
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+    
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size'][size]:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+    
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=200)
 # A session allows information to be stored until the client and server are done communicating.
 # Allowing me to store the contents in the shopping bag while in the HTTP session while a user browses the site and adds
 # items to be purchased, without the fear of losing all items in the bag.
