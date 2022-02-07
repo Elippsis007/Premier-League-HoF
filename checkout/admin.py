1041c78fdd676b7db16a317d9915423d2daad749
Elippsis007 <michaeldrmm@gmail.com>
2022-02-07 22:49:55 +0000

    added webhooks and imports

--- checkout/admin.py
+++ checkout/admin.py
@@ -9,21 +9,29 @@ class OrderLineItemAdminInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
+    
 
 # Fields that will be calculated by the model methods, these will not be editable by anyone
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag', 
+                       'stripe_pid')
+
+
 # This allow the specifity of the order of the fields in the admin console
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag', 
+              'stripe_pid')
+
+
 # Restricts columns that show in the order list to a minimal amout of key items
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
                     'grand_total',)
+
 
 # Sets the order to be in reverse by chronological order
     ordering = ('-date',)
