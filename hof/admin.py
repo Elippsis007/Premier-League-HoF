f1cb4d55eec869c755a28216df335c19508c2b82
Elippsis007 <michaeldrmm@gmail.com>
2022-02-08 20:08:31 +0000

    added fixtures to hof

--- hof/admin.py
+++ hof/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Hof
 
 # Register your models here.
+admin.site.register(Hof)
--- hof/models.py
+++ hof/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Hof(models.Model):
+    name = models.CharField(max_length=254)
+    quote = models.CharField(max_length=254)
+    quotee = models.CharField(max_length=254)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
