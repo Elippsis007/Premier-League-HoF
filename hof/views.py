2683e891940ae9d33824993c6cce9c6cecffc86a
Elippsis007 <michaeldrmm@gmail.com>
2022-02-09 19:48:01 +0000

    have for loop working

--- hof/views.py
+++ hof/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Hof
 
 # Create your views here.
 
 def hof_page(request):
     """ A view to show the Hall of Famers page """
 
-    return render(request, 'hof/hof_page.html')
+    hof = Hof.objects.all()
+
+    context = {
+        'hof': hof,
+    }
+
+    return render(request, 'hof/hof_page.html', context)
