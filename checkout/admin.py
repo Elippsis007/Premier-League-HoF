f79b3a066beb67e3917d1adcfbc329c67be16426
Elippsis007 <michaeldrmm@gmail.com>
2022-02-11 19:12:26 +0000

    added order history updated and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -19,7 +19,7 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 # This allow the specifity of the order of the fields in the admin console
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
