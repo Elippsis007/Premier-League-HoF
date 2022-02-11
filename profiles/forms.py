0128acd95a01b2fba24a91506d31eb484971504d
Elippsis007 <michaeldrmm@gmail.com>
2022-02-11 18:24:22 +0000

    added profile form, updated template

---
+++ profiles/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    # Meta options to tell django which model they will be connected with and which fields to be rendered
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    # Overriding the init which will allow customization
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+        }
+
+        # Starts the cursor in the "phone_number" field when the page is loaded by the user
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
