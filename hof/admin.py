7f2668fc71d367465da26b51b8ea28fdce8dfc7d
Elippsis007 <michaeldrmm@gmail.com>
2022-02-12 16:06:15 +0000

    fixed issue with .json file details not loading

--- hof/admin.py
+++ hof/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Hof
 
+class HofAdmin(admin.ModelAdmin):
+    list_display = (
+        'appearances',
+        'goals',
+        'assists',
+        'titles',
+    )
+
 # Register your models here.
 admin.site.register(Hof)
