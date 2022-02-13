759f85fd24d4d9797c7fc5deba00aca06f785d1e
Elippsis007 <michaeldrmm@gmail.com>
2022-02-13 17:05:12 +0000

    setting up with heroku for deployment

--- pl_hof/settings.py
+++ pl_hof/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 # DATABASES = {
 #     'default': dj_database_url.parse('postgres://cgngmjptoxxraj:fdd012b5c6f1b988e8c6c52afc51ccfadb4eb98f124e50eb80b3b398308ff122@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/d6nnsq4i9tuv17')
