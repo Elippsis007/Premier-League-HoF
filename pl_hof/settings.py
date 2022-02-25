ce1ca1315fdf430f56d015254cbe482faf8ea387
Elippsis007 <michaeldrmm@gmail.com>
2022-02-25 14:16:39 +0000

    finished env.py

--- pl_hof/settings.py
+++ pl_hof/settings.py
@@ -13,12 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-import os
 if os.path.exists("env.py"):
     import env
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
