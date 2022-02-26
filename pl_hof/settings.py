270def59ede413f2cdb14fc1c8d63e6dca749aa2
Elippsis007 <michaeldrmm@gmail.com>
2022-02-26 22:37:37 +0000

    DEBUG is now False for submission

--- pl_hof/settings.py
+++ pl_hof/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['premier-league-hof.herokuapp.com', 'localhost']
 
